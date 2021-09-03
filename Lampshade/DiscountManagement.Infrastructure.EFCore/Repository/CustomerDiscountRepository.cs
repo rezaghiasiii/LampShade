76c362d50cf96d27219bf71b17d9b927ad5e5480
Reza Ghiasi <imrezaghiasi@gmail.com>
2021-09-03 16:31:30 +0430

    Inventory Domain Implemented, Inventory Application & Repository Interface Created

--- Lampshade/DiscountManagement.Infrastructure.EFCore/Repository/CustomerDiscountRepository.cs
+++ Lampshade/DiscountManagement.Infrastructure.EFCore/Repository/CustomerDiscountRepository.cs
@@ -35,7 +35,7 @@ namespace DiscountManagement.Infrastructure.EFCore.Repository
 
         public List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel)
         {
-            var products = _shopContext.Products.Select(x => new {x.Id, x.Name}).ToList();
+            var products = _shopContext.Products.Select(x => new { x.Id, x.Name }).ToList();
             var query = _context.CustomerDiscounts.Select(x => new CustomerDiscountViewModel()
             {
                 Id = x.Id,
@@ -48,22 +48,26 @@ namespace DiscountManagement.Infrastructure.EFCore.Repository
                 Reason = x.Reason,
                 CreationDate = x.CreationDate.ToFarsi()
             });
+
             if (searchModel.ProductId > 0)
+            {
                 query = query.Where(x => x.ProductId == searchModel.ProductId);
+            }
 
             if (!string.IsNullOrWhiteSpace(searchModel.StartDate))
             {
-                query = query.Where(x => x.StartDateGr > searchModel.StartDate.ToGeorgianDateTime());
+                query = query.Where(x => x.StartDateGr >= searchModel.StartDate.ToGeorgianDateTime());
             }
+
 
             if (!string.IsNullOrWhiteSpace(searchModel.EndDate))
             {
-                query = query.Where(x => x.EndDateGr < searchModel.EndDate.ToGeorgianDateTime());
+                query = query.Where(x => x.EndDateGr <= searchModel.EndDate.ToGeorgianDateTime());
             }
 
             var discounts = query.OrderByDescending(x => x.Id).ToList();
 
-            discounts.ForEach(discount => discount.Product  = products.FirstOrDefault(x=>x.Id==discount.ProductId)?.Name);
+            discounts.ForEach(discount => discount.Product = products.FirstOrDefault(x => x.Id == discount.ProductId)?.Name);
 
             return discounts;
         }
