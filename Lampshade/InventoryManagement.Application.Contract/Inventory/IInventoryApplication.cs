f43580b445a9263a8ddbcfb5dbbbd8a6785ebe64
Reza Ghiasi <imrezaghiasi@gmail.com>
2021-09-03 16:39:05 +0430

    Reduce One Product From Inventory Method Added To IInventoryApplication

--- Lampshade/InventoryManagement.Application.Contract/Inventory/IInventoryApplication.cs
+++ Lampshade/InventoryManagement.Application.Contract/Inventory/IInventoryApplication.cs
@@ -8,7 +8,8 @@ namespace InventoryManagement.Application.Contract.Inventory
         OperationResult Create(CreateInventory command);
         OperationResult Edit(EditInventory command);
         OperationResult Increase(IncreaseInventory command);
-        OperationResult Decrease(List<DecreaseInventory> command);
+        OperationResult Reduce(List<ReduceInventory> command);
+        OperationResult Reduce(ReduceInventory command);
         EditInventory GetDetails(long id);
         List<InventoryViewModel> Search(InventorySearchModel searchModel);
     }
